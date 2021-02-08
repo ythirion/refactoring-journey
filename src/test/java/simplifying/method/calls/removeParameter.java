f291f9dfa76de0d47516541dfeda4983734fe0be
Yoan Thirion <yoan.thirion@gmail.com>
2021-02-08 14:13:01 +0100

    add removeParameter

---
+++ src/test/java/simplifying/method/calls/removeParameter.java
@@ -0,0 +1,47 @@
+package simplifying.method.calls;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.UUID;
+
+public class removeParameter {
+    private Lottery lottery;
+    private static final int TICKET_PRICE = 20;
+    private static final int PRIZE_AMOUNT = 109_908_786;
+
+    @BeforeEach
+    public void init() {
+        lottery = new Lottery();
+    }
+
+    @Test
+    public void lottery_allows_customer_to_purchase_a_ticket() {
+        UUID customerId = UUID.randomUUID();
+        String customerName = "Mando";
+        String ticketNumber = lottery.purchaseTicketForCustomer(customerId, customerName);
+
+        Assertions.assertEquals(6, ticketNumber.length());
+        Assertions.assertTrue(ticketNumber.matches("\\d+"));
+    }
+
+    @Test
+    public void lottery_throws_an_exception_on_draw_without_tickets() {
+        Assertions.assertThrows(IllegalStateException.class, () -> lottery.drawWinner(TICKET_PRICE, PRIZE_AMOUNT));
+    }
+
+    @Test
+    public void lottery_draws_a_winning_ticket() {
+        List<String> purchasedTickets = List.of(
+                lottery.purchaseTicketForCustomer(UUID.randomUUID(), "Customer 1"),
+                lottery.purchaseTicketForCustomer(UUID.randomUUID(), "Customer 2"),
+                lottery.purchaseTicketForCustomer(UUID.randomUUID(), "Customer 3"));
+
+        LotteryTicket winningTicket = lottery.drawWinner(TICKET_PRICE, PRIZE_AMOUNT);
+
+        Assertions.assertNotNull(winningTicket);
+        Assertions.assertTrue(purchasedTickets.contains(winningTicket.getNumber()));
+    }
+}
