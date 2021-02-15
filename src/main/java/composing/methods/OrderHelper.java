89746aea94297cc213db23321bbb8c772bedb836
Yoan Thirion <yoan.thirion@gmail.com>
2021-02-15 15:08:19 +0100

    Remove inline temps

--- src/main/java/composing/methods/OrderHelper.java
+++ src/main/java/composing/methods/OrderHelper.java
@@ -4,17 +4,15 @@ public class OrderHelper {
     private static final int MINIMUM_ITEMS_IN_STOCK = 10;
 
     public static boolean deserveDiscount(Order order) {
-        double price = order.totalPrice();
-        return price > 1000;
+        return order.totalPrice() > 1000;
     }
 
     // Customer deserves a discount if customer age / number of products < 5
     public static boolean deserveDiscountBasedOnCustomer(Order order) {
         double nbOfProducts = order.getProducts().size();
         int customerAge = order.getCustomer().getAge();
-        int result = (int) (customerAge / nbOfProducts);
 
-        return result < 5;
+        return (customerAge / nbOfProducts) < 5;
     }
 
     public static int calculateNewStock(Stock stock, int outFromStock) {
