4066c994040da4eacc5444e3d17373db6d74add2
Yoan Thirion <yoan.thirion@gmail.com>
2021-02-15 13:47:54 +0100

    Extract Methods

--- src/main/java/composing/methods/Order.java
+++ src/main/java/composing/methods/Order.java
@@ -17,21 +17,34 @@ public class Order {
         if (customer != null && !customer.getName().isEmpty() && products.size() > 0) {
             StringBuilder statement = new StringBuilder();
 
-            //Add banner
-            statement.append("Statement for : " + customer + "\n");
-
+            appendBanner(statement);
             for (Product p : products) {
-                // Add details.
-                statement.append("Product: " + p.getName() + " Price: " + p.getPrice() + "\n");
+                appendProductDetails(statement, p);
             }
             double total = AmountCalculator.calculatePrice(this, true, customer.getAge());
-            statement.append("Total: " + total + "€");
+
+            appendTotal(statement, total);
 
             return statement.toString();
         } else throw new IllegalArgumentException("InvalidOrder");
     }
 
+    private void appendBanner(StringBuilder statement) {
+        statement.append("Statement for : " + customer + "\n");
+    }
+
+    private void appendProductDetails(StringBuilder statement, Product p) {
+        statement.append("Product: " + p.getName() + " Price: " + p.getPrice() + "\n");
+    }
+
+    private void appendTotal(StringBuilder statement, double total) {
+        statement.append("Total: " + total + "€");
+    }
+
     public Double totalPrice() {
-        return getProducts().stream().map(Product::getPrice).reduce(0.0, Double::sum);
+        return getProducts()
+                .stream()
+                .map(Product::getPrice)
+                .reduce(0.0, Double::sum);
     }
 }
