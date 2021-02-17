1fed534e8d4da6fe28b4df093fb56af9b6f8572d
Yoan Thirion <yoan.thirion@gmail.com>
2021-02-17 13:59:05 +0100

    Replace Conditional with Polymorphism

--- src/main/java/simplifying/conditional/expressions/Calculator.java
+++ src/main/java/simplifying/conditional/expressions/Calculator.java
@@ -1,20 +1,27 @@
 package simplifying.conditional.expressions;
 
-public class Calculator {
-    public static int calculate(int a, int b, String operator) {
-        int result;
+import java.util.HashMap;
+import java.util.function.BiFunction;
 
-        if ("add".equals(operator)) {
-            result = a + b;
-        } else if ("multiply".equals(operator)) {
-            result = a * b;
-        } else if ("divide".equals(operator)) {
-            result = a / b;
-        } else if ("subtract".equals(operator)) {
-            result = a - b;
-        } else {
+public class Calculator {
+    public static final String ADD = "add";
+    public static final String MULTIPLY = "multiply";
+    public static final String DIVIDE = "divide";
+    public static final String SUBTRACT = "subtract";
+
+    private static final HashMap<String, BiFunction<Integer, Integer, Integer>> supportedOperations = new HashMap<>() {{
+        put(ADD, Integer::sum);
+        put(MULTIPLY, (a, b) -> a * b);
+        put(DIVIDE, (a, b) -> a / b);
+        put(SUBTRACT, (a, b) -> a - b);
+    }};
+
+    public static int calculate(int a,
+                                int b,
+                                String operator) {
+        if (!supportedOperations.containsKey(operator)) {
             throw new IllegalArgumentException("Not supported operator");
         }
-        return result;
+        return supportedOperations.get(operator).apply(a, b);
     }
 }
--- src/test/java/simplifying/conditional/expressions/replaceConditionalWithPolymorphism.java
+++ src/test/java/simplifying/conditional/expressions/replaceConditionalWithPolymorphism.java
@@ -8,25 +8,25 @@ import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 public class replaceConditionalWithPolymorphism {
     @Test
     public void calculator_should_support_add() {
-        int result = Calculator.calculate(1, 2, "add");
+        int result = Calculator.calculate(1, 2, Calculator.ADD);
         assertThat(result).isEqualTo(3);
     }
 
     @Test
     public void calculator_should_support_multiply() {
-        int result = Calculator.calculate(90, 4, "multiply");
+        int result = Calculator.calculate(90, 4, Calculator.MULTIPLY);
         assertThat(result).isEqualTo(360);
     }
 
     @Test
     public void calculator_should_support_divide() {
-        int result = Calculator.calculate(84, 4, "divide");
+        int result = Calculator.calculate(84, 4, Calculator.DIVIDE);
         assertThat(result).isEqualTo(21);
     }
 
     @Test
     public void calculator_should_support_subtract() {
-        int result = Calculator.calculate(84, 23, "subtract");
+        int result = Calculator.calculate(84, 23, Calculator.SUBTRACT);
         assertThat(result).isEqualTo(61);
     }
 
