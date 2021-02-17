0a416f682c83fc220202797d8be0d14839f4660e
Yoan Thirion <yoan.thirion@gmail.com>
2021-02-17 12:47:28 +0100

    Replace Magic Stuff with Constants

--- src/main/java/organizing/data/Utils.java
+++ src/main/java/organizing/data/Utils.java
@@ -1,13 +1,16 @@
 package organizing.data;
 
 public class Utils {
+    public static final int MINIMUM_PASSWORD_LENGTH = 12;
+    public static final double GRAVITATIONAL_CONSTANT = 9.81;
+
     public static void validatePassword(String password) {
-        if (password.length() < 12) {
+        if (password.length() < MINIMUM_PASSWORD_LENGTH) {
             throw new IllegalArgumentException("minimum password length is not respected");
         }
     }
 
     public static double calculatePotentialEnergy(double mass, double height) {
-        return mass * height * 9.81;
+        return mass * height * GRAVITATIONAL_CONSTANT;
     }
 }
