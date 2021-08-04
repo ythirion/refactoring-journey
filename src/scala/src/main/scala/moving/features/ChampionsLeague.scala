4ae0456042a89c5da27c5b864363a8a2cb05a965
Yoan Thirion <yoan.thirion@gmail.com>
2021-08-04 11:19:02 +0200

    add hide delegate

--- src/scala/src/main/scala/moving/features/ChampionsLeague.scala
+++ src/scala/src/main/scala/moving/features/ChampionsLeague.scala
@@ -2,5 +2,5 @@ package org.ythirion.refactoring.journey
 package moving.features
 
 case class ChampionsLeague(teams: List[Team]) {
-  def nextStageTable: String = teams.map(team => s"${team.name} - ${team.league.country}").mkString
+  def nextStageTable: String = teams.map(team => s"${team.name} - ${team.league.country}").mkString("\n")
 }
