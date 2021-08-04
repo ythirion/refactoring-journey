916838bdb2f3804516cc3ffdba4aaa8964af27d3
Yoan Thirion <yoan.thirion@gmail.com>
2021-08-04 13:50:25 +0200

    add pull up methods / fields

---
+++ src/scala/src/main/scala/dealing/with/generalization/TheaterPiece.scala
@@ -0,0 +1,35 @@
+package org.ythirion.refactoring.journey
+package dealing.`with`.generalization
+
+import scala.util.{Failure, Success, Try}
+
+case class TheaterPiece(description: String,
+                        numberOfActors: Int) extends Event {
+  private var running: Boolean = false
+  private var startTime: Long = 0
+  private var stopTime: Long = 0
+
+  def start(): Try[Boolean] = {
+    if (running) {
+      Failure(new IllegalStateException("Piece is already in progress"))
+    }
+    else {
+      startTime = System.currentTimeMillis()
+      running = true
+
+      Success(running)
+    }
+  }
+
+  def end(): Unit = {
+    if (running) {
+      running = false
+      stopTime = System.currentTimeMillis()
+    }
+  }
+
+  def calculateElapsedTime: Long = {
+    if (running) System.currentTimeMillis() - startTime
+    else stopTime - startTime
+  }
+}
--- src/scala/src/test/scala/simplifying/method/calls/useFactoryMethod.scala
+++ src/scala/src/test/scala/simplifying/method/calls/useFactoryMethod.scala
@@ -4,33 +4,27 @@ package simplifying.method.calls
 import org.scalatest.funsuite.AnyFunSuite
 
 class useFactoryMethod extends AnyFunSuite {
-  class notification_should extends AnyFunSuite {
-    class throw_an_illegal_argument_exception extends AnyFunSuite {
-      test("when channel is null") {
-        assertThrows[IllegalArgumentException](new Notification(null))
-      }
+  test("throw an illegal argument exception when channel is null") {
+    assertThrows[IllegalArgumentException](new Notification(null))
+  }
 
-      test("when channel is empty") {
-        assertThrows[IllegalArgumentException](new Notification(""))
-      }
+  test("throw an illegal argument exception when channel is empty") {
+    assertThrows[IllegalArgumentException](new Notification(""))
+  }
 
-      test("when channel is not authorized") {
-        assertThrows[IllegalArgumentException](new Notification("Unauthorized"))
-      }
-    }
+  test("throw an illegal argument exception when channel is not authorized") {
+    assertThrows[IllegalArgumentException](new Notification("Unauthorized"))
+  }
 
-    class instantiate extends AnyFunSuite {
-      test("when channel is SMS") {
-        assert(new Notification("SMS") != null)
-      }
+  test("instantiate when channel is SMS") {
+    assert(new Notification("SMS") != null)
+  }
 
-      test("when channel is EMAIL") {
-        assert(new Notification("EMAIL") != null)
-      }
+  test("instantiate when channel is EMAIL") {
+    assert(new Notification("EMAIL") != null)
+  }
 
-      test("when channel is PUSH") {
-        assert(new Notification("PUSH") != null)
-      }
-    }
+  test("instantiate when channel is PUSH") {
+    assert(new Notification("PUSH") != null)
   }
 }
