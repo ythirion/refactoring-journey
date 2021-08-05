1c818d7f1bf46385f55e3ad21d54512246636b8f
Yoan Thirion <yoan.thirion@gmail.com>
2021-08-05 11:04:33 +0200

    replace magic stuff with constants

--- src/scala/src/main/scala/organizing/data/Utils.scala
+++ src/scala/src/main/scala/organizing/data/Utils.scala
@@ -4,12 +4,15 @@ package organizing.data
 import scala.util.{Failure, Success, Try}
 
 object Utils {
+  val gravitationalConstant = 9.81
+  val minimumPasswordLength = 12
+
   def validatePassword(password: String): Try[Boolean] = {
     password match {
-      case p if p.length < 12 => Failure(new IllegalArgumentException("minimum password length is not respected"))
+      case p if p.length < minimumPasswordLength => Failure(new IllegalArgumentException("minimum password length is not respected"))
       case _ => Success(true)
     }
   }
 
-  def calculatePotentialEnergy(mass: Double, height: Double): Double = mass * height * 9.81
+  def calculatePotentialEnergy(mass: Double, height: Double): Double = mass * height * gravitationalConstant
 }
