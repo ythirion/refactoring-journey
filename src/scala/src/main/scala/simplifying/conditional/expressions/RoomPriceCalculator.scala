4187a7995d0e8f471018c4b65e54175cfbaa0351
Yoan Thirion <yoan.thirion@gmail.com>
2021-08-05 12:46:38 +0200

    Decompose Conditional

--- src/scala/src/main/scala/simplifying/conditional/expressions/RoomPriceCalculator.scala
+++ src/scala/src/main/scala/simplifying/conditional/expressions/RoomPriceCalculator.scala
@@ -9,23 +9,20 @@ class RoomPriceCalculator(val today: LocalDate,
                           val lowSeasonRate: Double,
                           val lowSeasonExtraCharge: Double) {
 
-  private val HIGH_SEASON_START_DATE = LocalDate.of(LocalDate.now.getYear, 6, 30)
-  private val HIGH_SEASON_END_DATE = LocalDate.of(LocalDate.now.getYear, 10, 31)
+  private val highSeasonStartDate = LocalDate.of(LocalDate.now.getYear, 6, 30)
+  private val highSeasonEndDate = LocalDate.of(LocalDate.now.getYear, 10, 31)
 
   def calculatePriceFor(numberOfRooms: Int,
                         selectedDate: LocalDate): Option[Double] = {
-    var price = .0
 
-    if (selectedDate.isAfter(today) && (selectedDate.isBefore(HIGH_SEASON_START_DATE) || selectedDate.isAfter(HIGH_SEASON_END_DATE))) {
-      if (numberOfRooms <= 0) return None
-      price = numberOfRooms * regularPrice * lowSeasonRate + lowSeasonExtraCharge
-    }
-    else if (selectedDate.isAfter(today)) {
-      price = numberOfRooms * regularPrice * highSeasonRate
-      if (numberOfRooms <= 0) return None
-    }
-    else return None
+    def isLowSeason = selectedDate.isBefore(highSeasonStartDate) || selectedDate.isAfter(highSeasonEndDate)
+    def calculatePrice(seasonPrice: Double => Double): Double = seasonPrice(numberOfRooms * regularPrice)
+    def lowSeasonPrice: Double = calculatePrice({ x => x * lowSeasonRate + lowSeasonExtraCharge })
+    def highSeasonPrice: Double = calculatePrice({ x => x * highSeasonRate })
 
-    Some(price)
+    if (selectedDate.isBefore(today) || numberOfRooms <= 0)
+      return None
+
+    Some(if (isLowSeason) lowSeasonPrice else highSeasonPrice)
   }
 }
